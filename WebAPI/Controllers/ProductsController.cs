62978d0860db664bf23b5a6b133a3975cc9b93b6
Khai Ngo Van <khaingovan2001@gmail.com>
2022-03-28 08:42:45 +0700

    BTTH

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPI.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        [HttpGet]
+        public List<tblSanPham> GetSanPhams()
+        {
+            DBCustomersDataContext db = new DBCustomersDataContext();
+            return db.tblSanPhams.ToList();
+        }
+        [HttpGet]
+        public tblSanPham GetProduct(string id)
+        {
+            DBCustomersDataContext db = new DBCustomersDataContext();
+            return db.tblSanPhams.FirstOrDefault(n => n.MaSP == id);
+        }
+        [Route("Search/{name}")]
+        [HttpGet]
+        public List<tblSanPham> GetSanPham(string name)
+        {
+            DBCustomersDataContext db = new DBCustomersDataContext();
+            return db.tblSanPhams.Where(n => n.TenSP.Contains(name)).ToList();
+        }
+        [Route("TonKho")]
+        [HttpGet]
+        public List<tblSanPham> GetTonKho()
+        {
+            DBCustomersDataContext db = new DBCustomersDataContext();
+            return db.tblSanPhams.Where(n => n.SoLuong > 0).ToList();
+        }
+        [HttpPost]
+        public bool InsertNewProduct(string id, string name, string description, decimal PriceIn, decimal PriceOut, int number)
+        {
+            try
+            {
+                DBCustomersDataContext dBProduct = new DBCustomersDataContext();
+                tblSanPham product = new tblSanPham();
+                product.MaSP = id;
+                product.TenSP = name;
+                product.MoTa = description;
+                product.GiaNhap = PriceIn;
+                product.GiaBan = PriceOut;
+                product.SoLuong = number;
+                dBProduct.tblSanPhams.InsertOnSubmit(product);
+                dBProduct.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        [HttpPut]
+        public bool UpdateCustomer(string id, string name, string description, decimal PriceIn, decimal PriceOut, int number)
+        {
+            try
+            {
+                DBCustomersDataContext dBProduct = new DBCustomersDataContext();
+                tblSanPham product = dBProduct.tblSanPhams.FirstOrDefault(n=>n.MaSP == id);
+                if(product == null) { return false; }
+                product.MaSP = id;
+                product.TenSP = name;
+                product.MoTa = description;
+                product.GiaNhap = PriceIn;
+                product.GiaBan = PriceOut;
+                product.SoLuong = number;
+                dBProduct.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        [HttpDelete]
+        public bool DeleteCustomers(string id)
+        {
+            try
+            {
+                DBCustomersDataContext dBProduct = new DBCustomersDataContext();
+                tblSanPham product = dBProduct.tblSanPhams.FirstOrDefault(n => n.MaSP == id);
+                if (product == null) { return false; }
+                dBProduct.tblSanPhams.DeleteOnSubmit(product);
+                dBProduct.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
--- WebAPI/DBCustomers.designer.cs
+++ WebAPI/DBCustomers.designer.cs
@@ -33,6 +33,9 @@ namespace WebAPI
     partial void InsertKHACHHANG(KHACHHANG instance);
     partial void UpdateKHACHHANG(KHACHHANG instance);
     partial void DeleteKHACHHANG(KHACHHANG instance);
+    partial void InserttblSanPham(tblSanPham instance);
+    partial void UpdatetblSanPham(tblSanPham instance);
+    partial void DeletetblSanPham(tblSanPham instance);
     #endregion
 		
 		public DBCustomersDataContext() : 
@@ -70,6 +73,14 @@ namespace WebAPI
 			get
 			{
 				return this.GetTable<KHACHHANG>();
+			}
+		}
+		
+		public System.Data.Linq.Table<tblSanPham> tblSanPhams
+		{
+			get
+			{
+				return this.GetTable<tblSanPham>();
 			}
 		}
 	}
@@ -183,6 +194,188 @@ namespace WebAPI
 					this._SDT = value;
 					this.SendPropertyChanged("SDT");
 					this.OnSDTChanged();
+				}
+			}
+		}
+		
+		public event PropertyChangingEventHandler PropertyChanging;
+		
+		public event PropertyChangedEventHandler PropertyChanged;
+		
+		protected virtual void SendPropertyChanging()
+		{
+			if ((this.PropertyChanging != null))
+			{
+				this.PropertyChanging(this, emptyChangingEventArgs);
+			}
+		}
+		
+		protected virtual void SendPropertyChanged(String propertyName)
+		{
+			if ((this.PropertyChanged != null))
+			{
+				this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+	}
+	
+	[global::System.Data.Linq.Mapping.TableAttribute(Name="dbo.tblSanPham")]
+	public partial class tblSanPham : INotifyPropertyChanging, INotifyPropertyChanged
+	{
+		
+		private static PropertyChangingEventArgs emptyChangingEventArgs = new PropertyChangingEventArgs(String.Empty);
+		
+		private string _MaSP;
+		
+		private string _TenSP;
+		
+		private string _MoTa;
+		
+		private System.Nullable<decimal> _GiaNhap;
+		
+		private System.Nullable<decimal> _GiaBan;
+		
+		private System.Nullable<int> _SoLuong;
+		
+    #region Extensibility Method Definitions
+    partial void OnLoaded();
+    partial void OnValidate(System.Data.Linq.ChangeAction action);
+    partial void OnCreated();
+    partial void OnMaSPChanging(string value);
+    partial void OnMaSPChanged();
+    partial void OnTenSPChanging(string value);
+    partial void OnTenSPChanged();
+    partial void OnMoTaChanging(string value);
+    partial void OnMoTaChanged();
+    partial void OnGiaNhapChanging(System.Nullable<decimal> value);
+    partial void OnGiaNhapChanged();
+    partial void OnGiaBanChanging(System.Nullable<decimal> value);
+    partial void OnGiaBanChanged();
+    partial void OnSoLuongChanging(System.Nullable<int> value);
+    partial void OnSoLuongChanged();
+    #endregion
+		
+		public tblSanPham()
+		{
+			OnCreated();
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_MaSP", DbType="NVarChar(5) NOT NULL", CanBeNull=false, IsPrimaryKey=true)]
+		public string MaSP
+		{
+			get
+			{
+				return this._MaSP;
+			}
+			set
+			{
+				if ((this._MaSP != value))
+				{
+					this.OnMaSPChanging(value);
+					this.SendPropertyChanging();
+					this._MaSP = value;
+					this.SendPropertyChanged("MaSP");
+					this.OnMaSPChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_TenSP", DbType="NVarChar(30)")]
+		public string TenSP
+		{
+			get
+			{
+				return this._TenSP;
+			}
+			set
+			{
+				if ((this._TenSP != value))
+				{
+					this.OnTenSPChanging(value);
+					this.SendPropertyChanging();
+					this._TenSP = value;
+					this.SendPropertyChanged("TenSP");
+					this.OnTenSPChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_MoTa", DbType="NVarChar(50)")]
+		public string MoTa
+		{
+			get
+			{
+				return this._MoTa;
+			}
+			set
+			{
+				if ((this._MoTa != value))
+				{
+					this.OnMoTaChanging(value);
+					this.SendPropertyChanging();
+					this._MoTa = value;
+					this.SendPropertyChanged("MoTa");
+					this.OnMoTaChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_GiaNhap", DbType="Money")]
+		public System.Nullable<decimal> GiaNhap
+		{
+			get
+			{
+				return this._GiaNhap;
+			}
+			set
+			{
+				if ((this._GiaNhap != value))
+				{
+					this.OnGiaNhapChanging(value);
+					this.SendPropertyChanging();
+					this._GiaNhap = value;
+					this.SendPropertyChanged("GiaNhap");
+					this.OnGiaNhapChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_GiaBan", DbType="Money")]
+		public System.Nullable<decimal> GiaBan
+		{
+			get
+			{
+				return this._GiaBan;
+			}
+			set
+			{
+				if ((this._GiaBan != value))
+				{
+					this.OnGiaBanChanging(value);
+					this.SendPropertyChanging();
+					this._GiaBan = value;
+					this.SendPropertyChanged("GiaBan");
+					this.OnGiaBanChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_SoLuong", DbType="Int")]
+		public System.Nullable<int> SoLuong
+		{
+			get
+			{
+				return this._SoLuong;
+			}
+			set
+			{
+				if ((this._SoLuong != value))
+				{
+					this.OnSoLuongChanging(value);
+					this.SendPropertyChanging();
+					this._SoLuong = value;
+					this.SendPropertyChanged("SoLuong");
+					this.OnSoLuongChanged();
 				}
 			}
 		}
